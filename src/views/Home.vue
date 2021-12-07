4a3e0bb9c58aa125b4cd844849235808fe832216
BabyBlackSkin <505627251@outlook.com>
2021-12-07 21:21:25 +0800

    项目搭建

---
+++ src/views/Home.vue
@@ -0,0 +1,120 @@
+<template>
+  <el-container>
+    <el-header>
+      <el-menu
+              :default-active="activeIndex2"
+              class="el-menu-demo"
+              mode="horizontal"
+              background-color="#545c64"
+              text-color="#fff"
+              active-text-color="#ffd04b"
+              @select="handleSelect"
+          >
+            <el-menu-item index="1">Processing Center</el-menu-item>
+            <el-sub-menu index="2">
+              <template #title>Workspace</template>
+              <el-menu-item index="2-1">item one</el-menu-item>
+              <el-menu-item index="2-2">item two</el-menu-item>
+              <el-menu-item index="2-3">item three</el-menu-item>
+              <el-sub-menu index="2-4">
+                <template #title>item four</template>
+                <el-menu-item index="2-4-1">item one</el-menu-item>
+                <el-menu-item index="2-4-2">item two</el-menu-item>
+                <el-menu-item index="2-4-3">item three</el-menu-item>
+              </el-sub-menu>
+            </el-sub-menu>
+            <el-menu-item index="3" disabled>Info</el-menu-item>
+            <el-menu-item index="4">Orders</el-menu-item>
+          </el-menu>
+    </el-header>
+    <el-container>
+      <el-aside width="200px">
+
+        <el-row class="tac">
+          <el-col>
+            <el-menu
+                active-text-color="#ffd04b"
+                background-color="#545c64"
+                class="el-menu-vertical-demo"
+                default-active="2"
+                text-color="#fff"
+                @open="handleOpen"
+                @close="handleClose"
+            >
+              <el-sub-menu index="1">
+                <template #title>
+                  <el-icon><location /></el-icon>
+                  <span>Navigator One</span>
+                </template>
+                <el-menu-item-group title="Group One">
+                  <el-menu-item index="1-1">item one</el-menu-item>
+                  <el-menu-item index="1-2">item one</el-menu-item>
+                </el-menu-item-group>
+                <el-menu-item-group title="Group Two">
+                  <el-menu-item index="1-3">item three</el-menu-item>
+                </el-menu-item-group>
+                <el-sub-menu index="1-4">
+                  <template #title>item four</template>
+                  <el-menu-item index="1-4-1">item one</el-menu-item>
+                </el-sub-menu>
+              </el-sub-menu>
+              <el-menu-item index="2">
+                <el-icon><icon-menu /></el-icon>
+                <span>Navigator Two</span>
+              </el-menu-item>
+              <el-menu-item index="3" disabled>
+                <el-icon><document /></el-icon>
+                <span>Navigator Three</span>
+              </el-menu-item>
+              <el-menu-item index="4">
+                <el-icon><setting /></el-icon>
+                <span>Navigator Four</span>
+              </el-menu-item>
+            </el-menu>
+          </el-col>
+        </el-row>
+      </el-aside>
+      <el-container>
+        <el-main>
+          <router-view/>
+        </el-main>
+        <el-footer>
+          <Footer></Footer>
+        </el-footer>
+      </el-container>
+    </el-container>
+  </el-container>
+
+</template>
+<script>
+// @ is an alias to /src
+import Footer from '@/components/Footer.vue'
+
+export default {
+  name: 'Home',
+  components: {
+    Footer
+  },
+  data() {
+    return {
+    }
+  }
+}
+</script>
+<style scoped lang="scss">
+.el-header{
+  padding: 0;
+}
+.el-container{
+  height: 100%;
+}
+.tac{
+  height: 100%;
+}
+.el-menu{
+  height: 100%;
+}
+::v-deep.el-menu--horizontal>.el-sub-menu .el-sub-menu__title,.el-menu--horizontal>.el-menu-item{
+  height: 59px;
+}
+</style>
---
+++ src/views/Index.vue
@@ -0,0 +1,127 @@
+<template>
+  <div class="login-container">
+    <h1 class="login-title">幕熙阁</h1>
+    <p class="login-desc">ADMIN DASHBOARD</p>
+    <form action="#" :model="login" class="login-form">
+      <label for="#">
+        <input type="text" v-model="login.account" placeholder="账户">
+      </label>
+      <label for="#">
+        <input type="text" v-model="login.pwd" placeholder="密码">
+      </label>
+      <label for="#" id="msg-code">
+        <input type="text" v-model="login.pwd" placeholder="验证码">
+        <input type="button" value="验证码">
+      </label>
+      <label for="#" id="login-contain">
+        <input type="button" value="登录" @click="loginDashBoard()">
+      </label>
+    </form>
+  </div>
+</template>
+
+<script>
+// @ is an alias to /src
+import HelloWorld from '@/components/HelloWorld.vue'
+
+export default {
+  name: 'Index',
+  components: {
+    HelloWorld
+  },
+  data() {
+    return {
+      login: {
+        account: "",
+        pwd: ""
+      },
+    }
+  },
+  methods: {
+    loginDashBoard() {
+      this.$router.replace({name: "Home"})
+    },
+  }
+}
+</script>
+
+<style scoped lang="scss">
+.login-container {
+  width: 25vw;
+  //height: 30vh;
+  background-color: white;
+  border-radius: 5px;
+  position: relative;
+  left: 50%;
+  margin-left: -12.5vw;
+  top: 20vh;
+  padding: 10px 0 20px 0;
+  box-shadow: 0 0 5px 3px #dedede;
+}
+
+.login-container h1 {
+  //margin-bottom: 5px;
+  padding: 5px 0 0 0;
+}
+
+.login-container p {
+  margin-bottom: 20px;
+  color: #666666;
+}
+
+.login-container .login-form {
+  width: 90%;
+  margin: 0 5%;
+}
+
+.login-container .login-form label {
+  display: inline-block;
+  width: 100%;
+  margin: 10px auto;
+}
+
+.login-form label input {
+  width: 100%;
+  height: 40px;
+  border: none;
+  border-radius: 3px;
+  background-color: #e9e9e9;
+  outline: none;
+  text-indent: 10px;
+  transition: all .35s;
+  color: #666666;
+
+  &:focus {
+    background-color: #d9d9d9;
+  }
+}
+
+#msg-code {
+  display: flex;
+  justify-content: space-between;
+}
+
+#msg-code input:nth-child(1) {
+  margin: 0;
+  width: 65%;
+}
+
+#msg-code input:nth-last-child(1) {
+  text-indent: 0;
+  margin: 0;
+  width: 30%;
+}
+
+.login-form label input[type='button'] {
+  cursor: pointer;
+  margin-top: 10px;
+  text-indent: 0;
+  color: white;
+  background-color: #666666;
+  box-shadow: 0 0 2px 0 #8f8f8f;
+
+  &:hover {
+    box-shadow: 0 5px 10px 0 #8f8f8f;
+  }
+}
+</style>
